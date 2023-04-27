cc627ec55b6bc6435f71cc5696e07e31bd5dd3fc
Anton <antybumer@gmail.com>
2023-04-27 17:24:34 +0300

    add readme.md and some style fix

---
+++ README.md
@@ -0,0 +1,22 @@
+# Test Sagirov (React, Scss, Vite)
+
+## Deployed project
+```
+https://test-sagirov.vercel.app
+```
+
+
+### Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css'
-import Button from 'react-bootstrap/Button'
 
 import Navbar from './components/Navbar';
 import Caption from './components/Caption';
 import Benefits from './components/Benefits';
 import Planet from './components/Planet';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <section className='mainContainer'>
